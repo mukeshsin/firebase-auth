16abb101728e412330985f5efc99a33e3fe32a02
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-01-10 19:26:56 +0530

    working on user profile

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -17,4 +17,20 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = createUser;
+const updateUserProfile = async (req, res) => {
+  
+    await admin.auth().onUpdate((user) => {
+
+  const db = admin.firestore();
+  // Create a new document in the 'users' collection with the user's UID as the document ID
+  return db.collection('users').doc(user.uid).set({
+    address: 'req.body.address',
+    profileImage: 'req.body.profileImage',
+    createdAt: admin.firestore.FieldValue.serverTimestamp()
+  });
+    
+
+}
+)};
+module.exports = (createUser,updateUserProfile)
+
