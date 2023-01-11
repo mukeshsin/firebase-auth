1bacdca999e9899f0c7cdac767121bc5f9aa8393
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-01-11 17:47:29 +0530

    Done addfriend

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,11 +3,12 @@ const express = require("express");
 
 const createUser = require("../controllers/auth.controllers.js");
 const updateUserProfile = require("../controllers/auth.controllers.js");
-
+const addFriend = require("../controllers/auth.controllers.js");
 // Init express router
 const router = express.Router();
 
 router.post("/add/User", createUser);
 router.put("/add/user/profile", updateUserProfile);
+router.post("/add/user/addUser",addFriend);
 
 module.exports = router;
