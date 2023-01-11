3613046f5f592d1afe61b1af8c9e417996c75dab
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-01-11 18:42:48 +0530

    chnged routes name

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.post("/add/User", createUser);
 router.put("/add/user/profile", updateUserProfile);
-router.post("/add/user/addUser",addFriend);
+router.post("/add/user/addfriend",addFriend);
 
 module.exports = router;
